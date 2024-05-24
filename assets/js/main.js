b4d87ead7f1bcb3d6d91c2fb72edbd7df8f7b535
SujalCodeCraft <sujal920501@gmail.com>
2024-05-24 16:58:17 +0530

    first  commit

---
+++ assets/js/main.js
@@ -0,0 +1,6 @@
+var typing = new Typed(".front-end", {
+    strings: ["", "(Front end developer)", "(Freelancer)", "( Web Designer )", "(Web Designer)", "(Web Developer )"],
+    typeSpeed: 100,
+    backSpeed: 100,
+    loop: true,
+});
